227c19dde009fe5feb8e729111b8d9e4b1ab0b22
Fazliddin <fxakimov020@gmail.com>
2023-10-06 20:18:11 +0500

    dffd

--- models/tourModel.js
+++ models/tourModel.js
@@ -48,6 +48,7 @@ const tourSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
+    select: false, //permanently hide but showed in mongodb
   },
   startDates: [Date],
 });
