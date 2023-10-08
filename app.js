8369db506f0ec1700b86a7a9b9df795ba8ae81de
Fazliddin <fxakimov020@gmail.com>
2023-10-08 19:05:02 +0500

    Error handling

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const e = require('express');
 const app = express();
 
 console.log('fd', process.env.NODE_ENV);
@@ -16,5 +18,11 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use('*', function (req, res, next) {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
